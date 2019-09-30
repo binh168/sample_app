14c79b92eda191224e0457c9942f634f0d32b237
AnBinh <thanhbinhbk888@gmail.com>
2019-09-30 17:40:11 +0700

    Make a basic User model (including secure passwords)_ver2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
 
-    
     private
     def downcase_email
         self.email=email.downcase
