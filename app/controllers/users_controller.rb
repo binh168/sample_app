d9686f9ed184ea8aa9ad2d4ad814392e6b623cc7
AnBinh <thanhbinhbk888@gmail.com>
2019-10-02 22:57:13 +0700

    Sign up ver4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      flash[:success] = t(".success_create_user")
+      flash[:success] = t(".success")
       redirect_to @user
     else
+      flash[:danger] = t(".error")
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by(params[:id])
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
-
-  def gravatar_for(user, size: Settings.size_gavatar)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  def gravatar_for user, size: Settings.size_gavatar 
+    gravatar_id = Digest::MD5::hexdigest user.email.downcase 
     gravatar_url = Settings.gavatar
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
   end
 end
