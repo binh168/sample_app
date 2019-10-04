8dfcc6d2e93ce224d31b7962d50299a5e3e94f21
AnBinh <thanhbinhbk888@gmail.com>
2019-10-04 16:28:11 +0700

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  before_action :load_user, only: %i(show)
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = t(".danger")
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
