9abeedfdd835a680dcc59610062a73241d13ec2e
AnBinh <thanhbinhbk888@gmail.com>
2019-10-21 14:06:08 +0700

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
   get "users/new"
   get "sessions/new"
+  
+  resources :users
+  resources :account_activations, only: %i(edit)
 end
