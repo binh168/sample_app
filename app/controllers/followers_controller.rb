b0f9f22e567916898f04871a88cd7235fe231f32
AnBinh <thanhbinhbk888@gmail.com>
2019-11-13 20:23:17 +0700

    Follower and Following Controller

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,15 @@
+class FollowersController < ApplicationController
+  before_action :logged_in_user
+  
+  def index
+    @title = t ".title_follower"
+    @user = User.find_by id: params[:id]
+    
+    if @user
+      @users = @user.following.page(params[:page]).per Settings.user_per_page
+    else
+      flash[:danger] = t ".danger"
+    end
+    render "users/show_follow"
+  end
+end
